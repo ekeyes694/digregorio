8f3f7f9401f3773fd58e9c1d477c9a8bd6e239c4
ekeyes694 <ekeyes693@gmail.com>
2016-10-20 22:12:16 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+var app = angular.module('myApp', ['ngRoute']);
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/home', {
+        templateUrl: 'partials/home.html',
+        controller: 'detailsController'
+    })
+        .when('/jen', {
+        templateUrl: 'partials/jen.html',
+        controller: 'chargesController'
+
+    })
+        .when('/amy', {
+        templateUrl: 'partials/amy.html',
+        controller: 'earningsController'
+    })
+        .when('/services', {
+        templateUrl: 'partials/services.html',
+        controller: 'earningsController'
+    })
+        .when('/contact', {
+        templateUrl: 'partials/contact.html',
+        controller: 'earningsController'
+    })
+        .otherwise({
+        redirectTo: '/home'
+    });
+
+}]);
+
+app.controller('navCtrl', function ($scope, $location) {
+    $scope.isActive = function (viewLocation) {
+        return viewLocation === $location.path();
+    };
+
+    $scope.classActive = function (viewLocation) {
+        if ($scope.isActive(viewLocation)) {
+            return 'active';
+        } else {
+            return 'inactive';
+        }
+    };
+});
